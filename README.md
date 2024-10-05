d0e63039749126ba9cce10c43cfd284ea180f017
as <allensnape@qq.com>
2024-10-06 00:07:21 +0800

    doc: refine

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Svelte Extra
 
 Common used components
+
+# Try It Out
+
+[https://allape.github.io/svelte-extra/index.html](https://allape.github.io/svelte-extra/index.html)
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,28 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 import Package from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), cssInjectedByJsPlugin()],
-  define: {
-    __APP_VERSION__: `"${Package.version}"`,
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        format: 'commonjs',
-        entryFileNames: 'app.js',
-        manualChunks: undefined,
-      },
-    },
-  },
+	plugins: [
+		svelte(),
+		// cssInjectedByJsPlugin(),
+		viteSingleFile()
+	],
+	define: {
+		__APP_VERSION__: `"${Package.version}"`
+	},
+	build: {
+		rollupOptions: {
+			context: 'svelte-ext',
+			output: {
+				format: 'commonjs',
+				entryFileNames: 'app.js',
+				manualChunks: undefined,
+				dir: 'docs',
+			}
+		}
+	}
 });
